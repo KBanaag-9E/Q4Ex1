bdca0da80766c2d4c6ed1380628e44bd1e61207b
Kylie Banana <banaag.kj@obmontessori.edu.ph>
2025-03-20 11:12:31 +0800

    Add files via upload

---
+++ myProject.js
@@ -0,0 +1,23 @@
+// round off
+	function round() {
+			var a = document.getElementById('a-value').value;
+			window.alert(Math.round(a));
+		}
+
+// squareroot of a
+	function absolute() {
+			var a = document.getElementById('a-value').value;
+			window.alert(Math.sqrt(a).toFixed(2));
+		}
+
+// absolute value
+	function squareRoot() {
+			var a = document.getElementById('a-value').value;
+			window.alert(Math.abs(a).toFixed(2));
+		}
+
+// a squared
+	function power() {
+			var a = document.getElementById('a-value').value;
+			window.alert(Math.pow(a, 2).toFixed(2));
+		}
